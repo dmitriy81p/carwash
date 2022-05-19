278a285c4b8769c964f111679fde044a5ef857d8
Dmitiry <dmitiy81p@gmail.com>
2022-05-19 09:54:56 +0300

    first commit

--- feedback.php
+++ feedback.php
@@ -1,39 +1,17 @@
 <?php
-
-$token = "5332594317:AAGIw2b5TB04wSaHaj7Pf5s3P73gpFm7N5o";
-
-//Сюда вставляем chat_id
-$chat_id = "-698734421";
-
-if ($_POST['act'] == 'order') {
-    $name = ($_POST['name']);
-    $email = ($_POST['email']);
-    $phone = ($_POST['phone']);
-    $news = ($_POST['news']);
-    $data = ($_POST['data']);
-
-    $arr = array(
-        'Имя:' => $name,
-        'Почта:' => $email,
-        'Телефон:' => $phone,
-        'Новости:' => $news,
-        'Обработка данных:' => $data
-    );
-
-
-    foreach($arr as $key => $value) {
-        $txt .= "<b>".$key."</b> ".$value."%0A";
-    };
-
-
-    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
-
-    if ($sendToTelegram) {
-        alert('Спасибо! Ваша заявка принята. Мы свяжемся с вами в ближайшее время.');
-    }
-
-    else {
-        alert('Что-то пошло не так. ПОпробуйте отправить форму ещё раз.');
-    }
-}
-?>
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
+} else {
+    echo "при отправке сообщения возникли ошибки";
+}?>
